0b28fc0a4b4404de57cb4acc50931388ed2bace2
Alexiy Rybin <alexiy.rybin@gmail.com>
2021-11-17 18:50:04 +0700

    Init

---
+++ README.md
@@ -0,0 +1,32 @@
+# Forwarding discord to telegram bot
+
+This bot can help you to read discord messages in telegam.
+
+## Setup
+
+### Disclaimer
+
+Self-Bots are against Discord's Terms of Service, so your account can be banned.
+
+1. Clone this project via
+
+   `git clone https://github.com/Zamur650/forwarding-discord-telegram.git`
+
+1. Create `.env` file
+
+   ```env
+   DISCORD_TOKEN=<YOUR_DISCORD_TOKEN>
+   TELEGRAM_TOKEN=<YOUR_TELEGAM_TOKEN>
+   TELEGRAM_CHAT_ID=<YOUR_TELEGRAM_CHAT_ID>
+   DISCORD_CHANNEL_ID=<YOUR_DISCORD_CHANNEL_ID>
+   ```
+
+1. Install dependencies via
+
+   `npm i` / `yarn` / `pnpm i`
+
+1. Run bot via
+
+   `npm run start` / `yarn start` / `pnpm run start`
+
+### 🎉 Now you have your bot running 🎉
---
+++ src/index.js
@@ -0,0 +1,69 @@
+const Discord = require('discord.js-selfbot')
+const { Telegraf } = require('telegraf')
+const dotenv = require('dotenv')
+
+;(async () => {
+	if (process.env.NODE_ENV != 'production') {
+		await dotenv.config()
+	}
+})()
+
+const client = new Discord.Client()
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', (message) => {
+	const date = new Date().toLocaleString('en-US', {
+		day: '2-digit',
+		year: '2-digit',
+		month: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit'
+	})
+
+	let toSend = `[${date}] [${message.guild.name} / ${message.channel.name} / ${message.author.tag}]: ${message.content}`
+
+	let allEmbeds = []
+
+	message.embeds.forEach((embed) => {
+		let stringEmbed = 'Embed:\n'
+
+		if (embed.title) stringEmbed += `  Title: ${embed.title}\n`
+		if (embed.description)
+			stringEmbed += `  Description: ${embed.description}\n`
+		if (embed.url) stringEmbed += `  Url: ${embed.url}\n`
+		if (embed.color) stringEmbed += `  Color: ${embed.color}\n`
+		if (embed.timestamp) stringEmbed += `  Url: ${embed.timestamp}\n`
+
+		let allFields = ['  Fields:\n']
+
+		embed.fields.forEach((field) => {
+			let stringField = '    Field:\n'
+
+			if (field.name) stringField += `      Name: ${field.name}\n`
+			if (field.value) stringField += `      Value: ${field.value}\n`
+
+			allFields = [...allFields, stringField]
+		})
+
+		if (allFields.length != 1) stringEmbed += `${allFields.join('')}`
+		if (embed.thumbnail) stringEmbed += `  Thumbnail: ${embed.thumbnail.url}\n`
+		if (embed.image) stringEmbed += `  Image: ${embed.image.url}\n`
+		if (embed.video) stringEmbed += `  Video: ${embed.video.url}\n`
+		if (embed.author) stringEmbed += `  Author: ${embed.author.name}\n`
+		if (embed.footer) stringEmbed += `  Footer: ${embed.footer.iconURL}\n`
+
+		allEmbeds = [...allEmbeds, stringEmbed]
+	})
+
+	if (allEmbeds.length != 0) toSend += allEmbeds.join('')
+
+	console.log(toSend)
+	bot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID, toSend)
+})
+
+client.login(process.env.DISCORD_TOKEN)
