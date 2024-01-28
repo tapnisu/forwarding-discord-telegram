9b18830bf94aea20a64fda146d69d8171f4769a8
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-01-28 15:42:01 +0700

    Separate into modules

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
+import { ChannelId } from "./config";
 
 export interface Env {
 	DISCORD_TOKEN: string;
 	TELEGRAM_TOKEN: string;
-	TELEGRAM_CHAT_ID: string | number;
+	TELEGRAM_CHAT_ID: ChannelId;
 }
 
-export function getEnv() {
+export function getEnv(): Env {
 	if (process.env.NODE_ENV != "production") dotenv.config();
 
 	return process.env as unknown as Env;
