985453ff6029e36445365c2af476f5879eee1c33
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-01-28 15:42:01 +0700

    Separate into modules

---
+++ src/bot.ts
@@ -0,0 +1,122 @@
+import { Client } from "discord.js-selfbot-v13";
+import { Config } from "./config";
+import { filterMessages } from "./filterMessages";
+import { formatSize } from "./format";
+import { SenderBot } from "./senderBot";
+
+export class Bot extends Client {
+	messagesToSend: string[] = [];
+	imagesToSend: string[] = [];
+	senderBot: SenderBot;
+	config: Config;
+
+	constructor(config: Config, senderBot: SenderBot) {
+		super();
+
+		this.config = config;
+		this.senderBot = senderBot;
+
+		this.on("ready", () => console.log(`Logged in as ${this.user?.tag}!`));
+
+		this.on("messageCreate", (message) => {
+			if (!filterMessages(message, this.config)) return;
+
+			const date = new Date().toLocaleString("en-US", {
+				day: "2-digit",
+				year: "2-digit",
+				month: "2-digit",
+				hour: "2-digit",
+				minute: "2-digit",
+				second: "2-digit"
+			});
+
+			let render = "";
+
+			if (config.showDate) render += `[${date}] `;
+			if (config.showChat)
+				render += message.inGuild()
+					? `[${message.guild.name} / ${message.channel.name} / ${message.author.tag}]: `
+					: `[${message.author.tag}]: `;
+
+			render += message.content;
+
+			const allAttachments: string[] = [];
+			const images: string[] = [];
+
+			const embeds = message.embeds.map((embed) => {
+				let stringEmbed = "Embed:\n";
+
+				if (embed.title) stringEmbed += `  Title: ${embed.title}\n`;
+				if (embed.description)
+					stringEmbed += `  Description: ${embed.description}\n`;
+				if (embed.url) stringEmbed += `  Url: ${embed.url}\n`;
+				if (embed.color) stringEmbed += `  Color: ${embed.color}\n`;
+				if (embed.timestamp) stringEmbed += `  Url: ${embed.timestamp}\n`;
+
+				const allFields = ["  Fields:\n"];
+
+				embed.fields.forEach((field) => {
+					let stringField = "    Field:\n";
+
+					if (field.name) stringField += `      Name: ${field.name}\n`;
+					if (field.value) stringField += `      Value: ${field.value}\n`;
+
+					allFields.push(stringField);
+				});
+
+				if (allFields.length != 1) stringEmbed += `${allFields.join("")}`;
+				if (embed.thumbnail)
+					stringEmbed += `  Thumbnail: ${embed.thumbnail.url}\n`;
+				if (embed.image) {
+					stringEmbed += `  Image: ${embed.image.url}\n`;
+
+					if (config.imagesAsMedia ?? true) images.push(embed.image.url);
+				}
+				if (embed.video) stringEmbed += `  Video: ${embed.video.url}\n`;
+				if (embed.author) stringEmbed += `  Author: ${embed.author.name}\n`;
+				if (embed.footer) stringEmbed += `  Footer: ${embed.footer.iconURL}\n`;
+
+				return stringEmbed;
+			});
+
+			if (embeds.length != 0) render += embeds.join("");
+
+			message.attachments.forEach((attachment) => {
+				if (
+					(config.imagesAsMedia ?? true) &&
+					attachment.contentType.startsWith("image")
+				)
+					return images.push(attachment.url);
+
+				allAttachments.push(
+					`Attachment:\n  Name: ${attachment.name}\n${
+						attachment.description
+							? `	Description: ${attachment.description}\n`
+							: ""
+					}  Size: ${formatSize(attachment.size)}\n  Url: ${attachment.url}`
+				);
+			});
+
+			if (allAttachments.length != 0) render += allAttachments.join("");
+
+			console.log(render);
+
+			if (config.stackMessages) {
+				this.messagesToSend.push(render);
+				this.imagesToSend.push(...images);
+
+				return;
+			}
+
+			this.senderBot.sendData([render], images);
+		});
+
+		if (config.stackMessages)
+			setInterval(() => {
+				this.senderBot.sendData(this.messagesToSend, this.imagesToSend);
+
+				this.messagesToSend.length = 0;
+				this.imagesToSend.length = 0;
+			}, 5000);
+	}
+}
---
+++ src/senderBot.ts
@@ -0,0 +1,51 @@
+import { autoRetry } from "@grammyjs/auto-retry";
+import { Bot, BotConfig, Context, InputMediaBuilder } from "grammy";
+import { ChannelId } from "./config";
+
+export class SenderBot<C extends Context = Context> extends Bot<C> {
+	chatsToSend: ChannelId[];
+	disableLinkPreview: boolean;
+
+	constructor(
+		token: string,
+		channelsToSend: ChannelId[],
+		disableLinkPreview: boolean,
+		config?: BotConfig<C>
+	) {
+		super(token, config);
+
+		this.chatsToSend = channelsToSend;
+		this.disableLinkPreview = disableLinkPreview;
+
+		this.api.config.use(autoRetry());
+
+		this.catch((err) => {
+			console.error(err);
+		});
+	}
+
+	async sendData(messagesToSend: string[], imagesToSend: string[]) {
+		try {
+			if (messagesToSend.length != 0) {
+				this.chatsToSend.forEach(async (chatId) => {
+					if (imagesToSend.length != 0)
+						await this.api.sendMediaGroup(
+							chatId,
+							imagesToSend.map((image) => InputMediaBuilder.photo(image))
+						);
+
+					if (messagesToSend.length == 0 || messagesToSend.join("") == "")
+						return;
+
+					await this.api.sendMessage(chatId, messagesToSend.join("\n"), {
+						link_preview_options: {
+							is_disabled: this.disableLinkPreview
+						}
+					});
+				});
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	}
+}
