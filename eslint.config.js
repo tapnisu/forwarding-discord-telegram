02f88125417e85ffa82edbb9579c64ac1d954d2c
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-07-23 11:56:22 +0700

    Migrate to v9 eslint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended
+];
