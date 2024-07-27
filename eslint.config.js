df291ce7cfc7f7ce975e1c18ce50f0b564348350
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-07-27 12:08:04 +0700

    Migrate .eslintignore to eslint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], ignores: ["dist"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
