16715cba129bd006d9af1300b082c8be9cb8b3ef
Aleksei Rybin <aleksei@tapni.su>
2024-10-14 18:47:13 +0700

    Use .gitignore for eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"], ignores: ["dist"] },
+  includeIgnoreFile(gitignorePath),
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
